4e769dc2d6d36908cae96cd7f164ab4ade1a00f5
reenboog <alex.gievsky@gmail.com>
2013-03-06 20:02:18 +0700

    Added project template

---
+++ sacraria_ios/GameLayer.h
@@ -0,0 +1,23 @@
+//
+//  HelloWorldLayer.h
+//  sacraria_ios
+//
+//  Created by Alex Gievsky on 06.03.13.
+//  Copyright spotGames 2013. All rights reserved.
+//
+
+
+#import <GameKit/GameKit.h>
+
+// When you import this file, you import all the cocos2d classes
+#import "cocos2d.h"
+
+// HelloWorldLayer
+@interface HelloWorldLayer : CCLayer <GKAchievementViewControllerDelegate, GKLeaderboardViewControllerDelegate>
+{
+}
+
+// returns a CCScene that contains the HelloWorldLayer as the only child
++(CCScene *) scene;
+
+@end
---
+++ sacraria_ios/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  sacraria_ios
+//
+//  Created by Alex Gievsky on 06.03.13.
+//  Copyright spotGames 2013. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppController");
+    [pool release];
+    return retVal;
+}
